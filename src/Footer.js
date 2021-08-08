de68d9c1804c36f4689632378266f338d49cbff2
Robbert Kooiman <robbert.kooiman@kubion.nl>
2021-08-08 19:21:09 +0100

    Version test

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Footer.scss';
+import packageJson from '../package.json';
 
 function Footer({ }) {
     return (
         <footer>
-            <p>Made by <a href="https://robbertkooiman.com">Rob</a> | Version 4</p>
+            <p>Made by <a href="https://robbertkooiman.com">Rob</a> | Version {packageJson.version}</p>
         </footer>
     );
 }
