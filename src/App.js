0e6f7810b1bdc20a1946b4ab48dd4fe0769139bb
Robbert Kooiman <robbert.kooiman@kubion.nl>
2021-08-11 18:14:40 +0100

    New dark theme

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ function App() {
   const [historyId, setHistoryId] = useState(0);
   const [history, setHistory] = useState([]);
 
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
   function calculateTime() {
     if (!!distance && !!split) {
       const newTime = split / SPLIT_DISTANCE * distance;
@@ -98,11 +100,11 @@ function App() {
   }, [split, time, distance])
 
   return (
-    <div className="App">
+    <div className={"App" + (prefersDarkScheme.matches ? " DarkMode" : "")}>
       <DistanceInput value={calcDistance} name="distance" type="text" title="Distance" placeholder="0000" onChange={e => setDistance(e)} />
       <DurationInput value={calcTime} name="time" type="duration" title="Time" pattern="[\d\:\.]*" onChange={e => setTime(e)} />
       <SplitInput value={calcSplit} name="split" type="split" title="Split / 500m" pattern="[\d\:\.]*" onChange={e => setSplit(e)} />
-      <p className="ClearButton"><Button href={void(0)} onClick={clearAll} className="Warning">clear all</Button></p>
+      <p className="ClearButton"><Button href={void (0)} onClick={clearAll} className="Warning">clear all</Button></p>
       <ButtonGroup>
         <Button onClick={calculateTime}>Time</Button>
         <Button onClick={calculateDistance}>Distance</Button>
